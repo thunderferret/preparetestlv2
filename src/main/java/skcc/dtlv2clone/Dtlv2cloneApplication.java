b9ede60f2adaa97eed6bf351b0baf556cf27821a
thunderferret <vpflt992@gmail.com>
2021-05-17 17:18:34 +0900

    first commit

---
+++ src/main/java/skcc/dtlv2clone/Dtlv2cloneApplication.java
@@ -0,0 +1,13 @@
+package skcc.dtlv2clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dtlv2cloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dtlv2cloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skcc/dtlv2clone/Dtlv2cloneApplicationTests.java
@@ -0,0 +1,13 @@
+package skcc.dtlv2clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dtlv2cloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
